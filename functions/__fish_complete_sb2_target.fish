adc06cce2039d615aa8da4d121ae7d181629f543
Dmitry Gerasimov <di.gerasimov@gmail.com>
2021-11-09 01:22:03 +0300

    Don't fail when there are no targets
    Prevent error from happening when there are no targets installed yet:
    
        sb2 -t ls: cannot access '/srv/mer/sdks/sfossdk/../../targets': No such file or directory

--- functions/__fish_complete_sb2_target.fish
+++ functions/__fish_complete_sb2_target.fish
@@ -1,7 +1,7 @@
 function __fish_complete_sb2_target
     if test "$SFOSSDK_USE_VM" = "y"
         sdk sb2-config -l 2>/dev/null | tr -d '\015'
-    else
+    else if test -d $SFOSSDK_PLATFORM_SDK_PATH/../../targets
         # Use faster approach when working locally
         ls $SFOSSDK_PLATFORM_SDK_PATH/../../targets
     end
